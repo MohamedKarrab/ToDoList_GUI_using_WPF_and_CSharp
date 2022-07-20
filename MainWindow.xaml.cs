044fbafd0e405a8680d1c46847ded9255a7c5b3d
Karrab <karrab2015@gmail.com>
2022-07-20 16:30:38 +0100

    Some optimisations

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -30,34 +30,10 @@ namespace ToDoList
             textBox = new TextBox();
             checkBox = new CheckBox();
         }
-        public void checkBox_Click(object sender, RoutedEventArgs e)
-        {
-            if (!completed)
-            {
-                completed = true;
-            }
-            else
-            {
-                completed = false;
-            }
-            if (completed)
-            {
-                textBox.Background = new SolidColorBrush(Colors.Green);
-            }
-            else
-            {
-                textBox.Background = new SolidColorBrush(Colors.White);
-            }
-        }
-
     }
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        TextCheckBox[] textCheckBoxes = new TextCheckBox[20];
-        int count = 0;
+        
         public MainWindow()
         {
             InitializeComponent();
@@ -65,27 +41,21 @@ namespace ToDoList
 
         private void AddTask_Click(object sender, RoutedEventArgs e)
         {
-            
-            
-
-            
-            
+    
             TextCheckBox tCB = new TextCheckBox();
             tCB.textBox.Text = "Type here";
             tCB.textBox.Width = 300;
-           // tB.Background = new SolidColorBrush(Colors.Green);
+          
             List.Items.Add(tCB.textBox);
            tCB.checkBox.Margin = new Thickness(0,6.8f,0,0);
             checkBoxList.Items.Add(tCB.checkBox);
-            //textBoxes[0].Background = new SolidColorBrush(Colors.Green);
-            textCheckBoxes[count] = tCB;
-            count++;
-
+            
             MessageBoxButton mBBTN = MessageBoxButton.YesNo;
             string caption = "Add Task";
             MessageBoxImage mBIcon = MessageBoxImage.Asterisk;
             MessageBoxResult mBResult = MessageBoxResult.Yes;
             mBResult = MessageBox.Show("Task added, do you want to add another task?", caption, mBBTN, mBIcon, mBResult);
+            
             switch (mBResult)
             {
                 case MessageBoxResult.Yes:
